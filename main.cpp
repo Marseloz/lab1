13bbf991413fd57623399ae22573e5539fec9f11
Marsel <44693100+Marseloz@users.noreply.github.com>
2021-05-07 20:14:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
-#include "class.h"
+#include "class.h";
 #include <iostream>;
-#include <fstream>;
+#include <string>;
+
+using namespace std;
 
 
 int main() {
 	TM a;
 	a.the_date();
-	std::cout << a.DateFromString("DD/YYYY/MM");
+	cout << a.DateToString("DD/YYYY/MM");
 
-	std::ifstream file("text.txt");
-	std::string s;
-	std::getline(file, s);
-	file.close();
-	std::ofstream file2("text.txt", std::ios_base::out | std::ios_base::trunc);
-	file2 << a.DateFromString(s);
+	string f = "ffff 4 fbfh 02.12.12-13:12:2013 fhg";
+	string h = "ss.mm.hh-DD:MM:YYYY";
+	cout << endl;
+	a.dat_date(f, h).Print(h);
+
+	//std::ifstream file("text.txt");
+	//std::string s;
+	//std::getline(file, s);
+	//file.close();
+	//std::ofstream file2("text.txt", std::ios_base::out | std::ios_base::trunc);
+	//file2 << a.DateToString(s);
 	return 0;
 }
